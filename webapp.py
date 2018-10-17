14be030170c3e5f8f9d02018dd1e63ae3f61c497
ARandomGuy1 <42652957+ARandomGuy1@users.noreply.github.com>
2018-10-17 11:32:53 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,30 @@
+from flask import Flask, url_for, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+
+@app.route("/p1")
+def render_page1():
+    return render_template('page1.html')
+
+@app.route("/p2")
+def render_page2():
+    return render_template('page2.html')
+
+@app.route("/p3")
+def render_page3():
+    return render_template('page3.html')
+
+  
+@app.route("/response")
+def render_response():
+    foot = request.args['foot']
+    return render_template('response.html', response = float(foot)*30.48)
+                      
+   
+  
+if __name__=="__main__":
+    app.run(debug=False, port=54321)
